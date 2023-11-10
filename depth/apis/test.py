1c08e8ae57a744b042453b2bbacccdbc7e93ac28
Casimir-Feldmann <cfeldmann@student.ethz.ch>
2023-11-10 17:55:05 +0100

    image saving fix

--- depth/apis/test.py
+++ depth/apis/test.py
@@ -137,7 +137,7 @@ def single_gpu_test(model,
                         filename = filename + '.npy'
                         out_file = osp.join(out_dir, filename)
                     else:
-                        out_file = osp.join(out_dir, replace_str(img_meta['ori_filename']))
+                        out_file = osp.join(out_dir, replace_str(img_meta['ori_filename'].split("/")[-1]))
                 else:
                     out_file = None
 
